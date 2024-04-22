94b4397c57eae71b7e0efaf78b8d1b9139fdc53f
NayZawAg <69493814+NayZawAg@users.noreply.github.com>
2024-04-22 13:15:41 +0900

    Updating files into oracle_client files

---
+++ roles/oracle_client/files/oracle.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export ORACLE_HOME=/usr/lib/oracle/12.1/client64
+export PATH=$ORACLE_HOME/bin:$PATH
+export DYLD_LIBRARY_PATH=$ORACLE_HOME/lib
+export OCI_DIR=$ORACLE_HOME
+export LD_LIBRARY_PATH=$ORACLE_HOME/lib
+export NLS_LANG=Japanese_Japan.AL32UTF8
